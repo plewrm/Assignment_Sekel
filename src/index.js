2acddff6f50bb326f3d086bae812b9f35faf6b43
pawan <salvepawan3@gmail.com>
2024-05-08 16:50:01 +0530

    Favorite list refresh

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<Provider store={store} >
-<App/>
-</Provider>
- 
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>,
+
   </React.StrictMode>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import productReducer from './reducers/ProductReducer';
 import favoriteReducer from './reducers/FavoriteReducer';
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 
 const rootReducer = combineReducers({
@@ -9,6 +17,8 @@ const rootReducer = combineReducers({
   favorites: favoriteReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
 export default store;
